5d488f4cd9834c9ae4759cb849f3fbabc7dfed32
manzomonza <massimiliano.manzo@posteo.ch>
2022-12-07 14:41:35 +0100

    Add test for clonesummary indexing

--- tests/testthat/test-clonesummary_indexing.R
+++ tests/testthat/test-clonesummary_indexing.R
@@ -4,5 +4,17 @@ testthat::test_that(desc = "Correct indexing of panel",
                      testthat::expect_equal(clonesummary_indexing(charstring = inputstring),
                                           c("BCR",1)
                      )
+                     inputstring = 'BCR_K_L'
+                     testthat::expect_equal(clonesummary_indexing(charstring = inputstring),
+                                            c("BCR",2)
+                                            )
+                     inputstring = 'TCR_beta'
+                     testthat::expect_equal(clonesummary_indexing(charstring = inputstring),
+                                            c("TCR",2) 
+                                            )
+                     inputstring = 'TCR_gamma'
+                     testthat::expect_equal(clonesummary_indexing(charstring = inputstring),
+                                            c("TCR",1)
+                                            )
                    }
-                   )
+)
