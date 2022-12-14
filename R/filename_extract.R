5f0692adc5d262a9e2466bfea93b0ac66496ab51
manzomonza <massimiliano.manzo@posteo.ch>
2022-12-14 13:46:25 +0100

    Extract largest substring from both clonesummaries

---
+++ R/filename_extract.R
@@ -0,0 +1,26 @@
+#' Extract common string from filenames
+#'
+#' @param filevector 
+#'
+#' @return
+#' @export
+#'
+#' @examples banana AND ananas -> anana
+filename_extract <- function(file1, file2){
+  i = 10
+  #print(file1)
+  while(i <=(nchar(file2))){
+    testme = substr(file1, start = 1,stop =i)
+    test_substring = stringr::str_detect(file2, testme)
+    #print(test_substring)
+    if(test_substring){
+      i = i+1
+    }else{
+      return(testme)
+    }
+  }
+}
+
+
+
+
