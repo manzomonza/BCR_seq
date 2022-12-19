14cb2b6480b07aa2ea114cc91d6a0b40fb22a216
manzomonza <massimiliano.manzo@posteo.ch>
2022-12-19 12:38:37 +0100

    Remove output string of topClone

--- R/topClone.R
+++ R/topClone.R
@@ -12,8 +12,7 @@ topClone = function(clonesum){
   freq = clonesum[which(clonesum$rank == 1)]$frequency
   freq = as.numeric(freq)
   freq = round(freq*100, 1)
-  stringoi = paste0("Largest clone frequency: ", freq, "%" )
-  return(stringoi)
+  return(freq)
 }
 
 
