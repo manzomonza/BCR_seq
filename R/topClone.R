eae370ba003afc9f8c9237fadbc94eafa164bb04
manzomonza <massimiliano.manzo@posteo.ch>
2022-12-19 11:54:57 +0100

    topClone *100 and round

--- R/topClone.R
+++ R/topClone.R
@@ -10,7 +10,8 @@
 #' @examples
 topClone = function(clonesum){
   prod_freq = clonesum[which(clonesum$rank == 1)]$productive_frequency
-  prod_freq = round(prod_freq,1)
+  prod_freq = round(as.numeric(prod_freq)*100,1)
   stringoi = paste0("Largest clone frequency: ",prod_freq, "%" )
   return(clonesum)
 }
+
