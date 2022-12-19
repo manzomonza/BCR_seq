802c9957a794deec52f455b6c3a1534873567860
manzomonza <massimiliano.manzo@posteo.ch>
2022-12-19 12:13:33 +0100

    Correct return of topClone

--- R/topClone.R
+++ R/topClone.R
@@ -12,6 +12,6 @@ topClone = function(clonesum){
   prod_freq = clonesum[which(clonesum$rank == 1)]$productive_frequency
   prod_freq = round(as.numeric(prod_freq)*100,1)
   stringoi = paste0("Largest clone frequency: ",prod_freq, "%" )
-  return(clonesum)
+  return(stringoi)
 }
 
