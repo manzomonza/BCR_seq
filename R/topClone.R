e06215c06575131ab37f14f95c77d05b13f8b063
manzomonza <massimiliano.manzo@posteo.ch>
2023-01-12 14:01:20 +0100

    Add summary table to beginning of report

--- R/topClone.R
+++ R/topClone.R
@@ -15,6 +15,24 @@ topClone = function(clonesum){
   return(freq)
 }
 
+#' Retrieve the division result of i and jth clone frequencies
+#'
+#' @param clonesum
+#'
+#' @return Return Frequency and sequence of top clone
+#' @export
+#'
+#' @examples
+topClone_analysis_clonesum = function(clonesum, i, j){
+  freq_nom = as.numeric(clonesum[i,]$frequency)
+  freq_denom = as.numeric(clonesum[j,]$frequency)
+  ratio = freq_nom/freq_denom
+  ratio = round(ratio, digits = 2)
+  return(data.frame(nom = freq_nom,
+              denom = freq_denom,
+              ratio = ratio))
+}
+
 
 
 #' Retrieve the division result of i and jth clone
@@ -25,12 +43,12 @@ topClone = function(clonesum){
 #' @export
 #'
 #' @examples
-topClone_analysis_lineage = function(clonesum, i, j){
-  freq_nom = as.numeric(clonesum[i,]$lineage_frequency)
-  freq_denom = as.numeric(clonesum[j,]$lineage_frequency)
+topClone_analysis_lineage = function(lineage, i, j){
+  freq_nom = as.numeric(lineage[i,]$lineage_frequency)
+  freq_denom = as.numeric(lineage[j,]$lineage_frequency)
   ratio = freq_nom/freq_denom
   ratio = round(ratio, digits = 2)
-  return(list(nom = freq_nom,
+  return(data.frame(nom = freq_nom,
               denom = freq_denom,
               ratio = ratio))
 }
