2ce1e6aeb09c46cb302bfeaf2c1f52f22eb89eeb
manzomonza <massimiliano.manzo@posteo.ch>
2023-01-12 14:45:25 +0100

    Add top25 selection function

--- R/topClone.R
+++ R/topClone.R
@@ -52,4 +52,36 @@ topClone_analysis_lineage = function(lineage, i, j){
               ratio = ratio))
 }
 
+#' Return top25 clones of lineage summaries formatted for presentation in Data Table
+#'
+#' @param lineage_file
+#'
+#' @return
+#' @export
+#'
+#' @examples
+top25_lineage_selection <- function(lineage_file){
+  selection = head(lineage_file, n = 25) %>%
+    dplyr::select(lineage_id, variable,top_cdr3aa,lineage_frequency,number_of_clones)
+  colnames(selection) = gsub("_", " ",stringr::str_to_title(colnames(selection)))
+  return(selection)
+}
+
+#' Return top25 clones of lineage summaries formatted for presentation in Data Table
+#'
+#' @param lineage_file
+#'
+#' @return
+#' @export
+#'
+#' @examples
+top25_clonesum_selection <- function(clonesum){
+  selection = head(clonesum, n = 25) %>%
+    dplyr::select(variable,top_cdr3aa,frequency)
+  colnames(selection) = gsub("_", " ",stringr::str_to_title(colnames(selection)))
+  return(selection)
+}
+
+
+
 
