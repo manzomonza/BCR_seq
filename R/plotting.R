ee25a82a6009bdbcb597c663d6297fbbd2124ec2
manzomonza <massimiliano.manzo@posteo.ch>
2023-01-12 15:40:27 +0100

    Add plotting functionality

--- R/plotting.R
+++ R/plotting.R
@@ -1,10 +1,55 @@
 # PLOT
 
 
-clone_frequencies <- function(){}
-clone_frequencies_vs_rank <- function(){}
-strandbias <- function(){}
+#' Plot clone frequencies vs productive/unproductive category
+#'
+#' @param plot_prepped
+#'
+#' @return
+#' @export
+#'
+#' @examples
+clone_frequencies_clonsum <- function(plot_prepped){
+  gplot <- ggplot(plot_prepped, aes(x=functionality, y=frequency, color = top)) +
+    geom_jitter(width=0.25, alpha=0.7, size=2.5) + ylim (0,0.50) +
+    geom_hline(yintercept=0.1, linetype="dashed", color = "red", size=1) +
+    geom_hline(yintercept=0.01, linetype="dashed", color = "blue",size=1) +
+    ggtitle("Clone Frequency") + theme(plot.title = element_text(size=12, face="bold"),
+                                       axis.title.x = element_blank(),
+                                       axis.title.y = element_text(),
+                                       legend.position = "none")
+  return(gplot)
+}
 
+#' Plot lineage frequencies vs productive/unproductive category
+#'
+#' @param plot_prepped
+#'
+#' @return
+#' @export
+#'
+#' @examples
+clone_frequencies_clonsum <- function(plot_prepped){
+  gplot <- ggplot(plot_prepped, aes(x=functionality, y=lineage_frequency, color = top)) +
+    geom_jitter(width=0.25, alpha=0.7, size=2.5) + ylim (0,0.50) +
+    geom_hline(yintercept=0.1, linetype="dashed", color = "red", size=1) +
+    geom_hline(yintercept=0.01, linetype="dashed", color = "blue",size=1) +
+    ggtitle("Clone Frequency") + theme(plot.title = element_text(size=12, face="bold"),
+                                       axis.title.x = element_blank(),
+                                       axis.title.y = element_text(),
+                                       legend.position = "none")
+  return(gplot)
+}
+
+
+#' Prepare summary file for plotting
+#'
+#' @param summaryfile
+#'
+#' @return
+#' @export
+#'
+#' @examples
 plot_prep <- function(summaryfile){
   sumfile = clonesum_read(summaryfile) |>
     dplyr::mutate(top = if_else(rank == 1, TRUE, FALSE)) %>%
@@ -12,6 +57,14 @@ plot_prep <- function(summaryfile){
   return(sumfile)
 }
 
+#' Plot total read counts with productive/unproductive separation
+#'
+#' @param plot_prepped
+#'
+#' @return
+#' @export
+#'
+#' @examples
 total_read_counts <- function(plot_prepped){
     gplot = ggplot(plot_prepped , aes(x=functionality, y=total_counts, color=top)) +
       geom_jitter(width=0.25, alpha=0.7, size=2.5)  +
@@ -21,4 +74,63 @@ total_read_counts <- function(plot_prepped){
                                            legend.position = "none")
     return(gplot)
 }
+
+#' Plot clone frequencies vs rank -- clone summary file
+#'
+#' @param plot_prepped
+#'
+#' @return
+#' @export
+#'
+#' @examples
+clone_frequencies_vs_rank_clonesum <- function(plot_prepped){
+  gplot<-ggplot(plot_prepped, aes(x=rank, y=frequency, color = top)) +
+    geom_point() +
+    scale_x_log10() +
+  geom_hline(yintercept=0.01, linetype="dashed", size=1, color = "blue") +
+  ggtitle("Clone Frequency versus Rank") + theme(plot.title = element_text(size=12, face="bold"),
+                                                 legend.position = "none") +
+  labs(x = "Rank", y="Frequency")
+  return(gplot)
+}
+
+#' Plot clone frequencies vs rank -- lineage summary file
+#'
+#' @param plot_prepped
+#'
+#' @return
+#' @export
+#'
+#' @examples
+clone_frequencies_vs_rank_lineage <- function(plot_prepped){
+  gplot<-ggplot(plot_prepped, aes(x=rank, y=frequency, color = top)) +
+    geom_point() +
+    scale_x_log10() +
+    geom_hline(yintercept=0.01, linetype="dashed", size=1, color = "blue") +
+    ggtitle("Clone Frequency versus Rank") + theme(plot.title = element_text(size=12, face="bold"),
+                                                   legend.position = "none") +
+    labs(x = "Rank", y="Frequency")
+  return(gplot)
+}
+
+
+
+#' Plot Strand bias versus rank
+#'
+#' @param plot_prepped
+#'
+#' @return
+#' @export
+#'
+#' @examples
+strandbias_plot <- function(plot_prepped){
+  gplot = plot_prepped %>%
+  ggplot(aes(rank, log2_strand)) +
+  geom_point(aes(col = top)) +
+  scale_x_log10() +
+  ggtitle("Strand bias") + theme(plot.title = element_text(size=12, face="bold"),
+                                legend.position = "none")
+  return(gplot)
+}
+
 
