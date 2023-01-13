c6d77c75b49577fe56230c3ba0314cf6052466a9
manzomonza <massimiliano.manzo@posteo.ch>
2023-01-13 11:48:17 +0100

    Remove empty string check in clonesum_read

--- R/clonesummaries_read.R
+++ R/clonesummaries_read.R
@@ -2,19 +2,15 @@
 
 #' Read  clone summary file
 #'
-#' @param filepath 
+#' @param filepath
 #'
 #' @return Read in clone summary with filepath as additional column
 #' @export
 #'
 #' @examples
 clonesum_read <- function(inputpath){
-  if(length(list.files(inputpath) == 0)){
-    stop(paste0("Empty string"))
-  }else{
   clonesum = data.table::fread(inputpath)
   clonesum = janitor::clean_names(clonesum)
   clonesum$filepath = inputpath
   return(clonesum)
-  }
 }
